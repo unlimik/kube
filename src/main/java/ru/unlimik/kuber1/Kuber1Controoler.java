1e7da5db7bac3a75fc075b7ace9230041b116646
unlimik <unlimik@mail.ru>
2021-04-19 16:09:48 +0400

    Update Kuber1Controoler.java

--- src/main/java/ru/unlimik/kuber1/Kuber1Controoler.java
+++ src/main/java/ru/unlimik/kuber1/Kuber1Controoler.java
@@ -10,6 +10,6 @@ public class Kuber1Controoler {
     @ResponseBody
     String hello()
     {
-        return "Hello! Hello! Hello! Hi!";
+        return "Hello! Hello! Hello! Hi! Moscow!";
     }
 }
